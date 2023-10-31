69c3f2d0e65a6947587f9fd2aef7314b43e195f6
amanzanares19 <amanzanares19@ilg.cat>
2023-10-31 18:48:02 +0100

    added Serializable

---
+++ M8/M08Ejercicios/Seriazable/app/src/main/java/com/example/seriazable/AnimeSer.java
@@ -0,0 +1,93 @@
+package com.example.seriazable;
+
+import java.io.Serializable;
+
+public class AnimeSer implements Serializable {
+
+    private int id;
+    private String name;
+    private String description;
+    private String type;
+    private int year;
+    private String image;
+    private String favorite;
+
+    public AnimeSer(int id, String name, String description, String type, int year, String image, String favorite) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.type = type;
+        this.year = year;
+        this.image = image;
+        this.favorite = favorite;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getFavorite() {
+        return favorite;
+    }
+
+    public void setFavorite(String favorite) {
+        this.favorite = favorite;
+    }
+
+    @Override
+    public String toString() {
+        return "AnimeSer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", type='" + type + '\'' +
+                ", year=" + year +
+                ", image='" + image + '\'' +
+                ", favorite='" + favorite + '\'' +
+                '}';
+    }
+}
