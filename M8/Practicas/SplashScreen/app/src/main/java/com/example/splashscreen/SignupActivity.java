6670c9866350176573cb048d2c60830b06810219
amanzanares19 <amanzanares19@ilg.cat>
2023-11-02 10:04:29 +0100

    Added DatePicker and Spinner working Splash Screen

--- M8/Practicas/SplashScreen/app/src/main/java/com/example/splashscreen/SignupActivity.java
+++ M8/Practicas/SplashScreen/app/src/main/java/com/example/splashscreen/SignupActivity.java
@@ -2,6 +2,8 @@ package com.example.splashscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -10,11 +12,19 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
 import android.view.animation.ScaleAnimation;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class SignupActivity extends AppCompatActivity {
 
     private TextView login;
+
+    private DatePickerDialog picker;
+    private EditText textDate;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +33,7 @@ public class SignupActivity extends AppCompatActivity {
 
         //hooks
         login = findViewById(R.id.login);
+        textDate = (EditText) findViewById(R.id.birthdate);
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,6 +67,27 @@ public class SignupActivity extends AppCompatActivity {
                 Handler h = new Handler(Looper.getMainLooper());
                 h.postDelayed(r, DashboardActivity.SPLASH_SCREEN);
 
+            }
+        });
+
+        //obrim la finestra DatePickerDialog i mostrem el que hem escollit
+        textDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Guardem el dia actual amb Calendar.getInstance()
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                picker = new DatePickerDialog(SignupActivity.this, android.R.style.Theme_Holo_Dialog,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view,int year,int monthOfYear,int dayOfMonth) {
+                                textDate.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                            }
+                        }, year, month, day);
+                picker.show();
             }
         });
 
