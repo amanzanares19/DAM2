ea2f153cb7c511d605ec9ce4f9f169ba58902779
amanzanares19 <amanzanares19@ilg.cat>
2024-02-22 18:02:58 +0100

    Exercici 1 M10 finished

--- M10/Exercicis Evaluables/AC02/client.py
+++ M10/Exercicis Evaluables/AC02/client.py
@@ -3,28 +3,33 @@ import re
 
 class Client:
     
-    contador = 0
     
     def __init__(self, id, nom, cognom, telefon, correu, ciutat):
-        
-        #Add id if not exists
-        if not id in self.idList:            
-            self.__id = id
-            self.contador = self.contador + 1
+    
+        #Add id if not exists            
+        self.id = id          
             
-        if nom.isalpha() & cognom.isalpha():
-            self.__nom = nom
-            self.__cognom = cognom
+        if re.match("^[A-Z][a-z]*$", nom) and re.match("^[A-Z][a-z]*$", cognom):
+            self.nom = nom
+            self.cognom = cognom  
+        else:
+            raise Exception("Nombre o apellido incorrecto")      
         
-        if re.match("\+00[0-9]{2}-[0-9]{6,10}", telefon) | re.match("[0-9]{6,10}", telefon):
-            self.__telefon = telefon
+        if re.match("\+00[0-9]{2}-[0-9]{6,10}", telefon) or re.match("[0-9]{6,10}", telefon):
+            self.telefon = telefon                     
+        else:
+            raise Exception("Telefono no sigue el formato")      
         
         if re.match("^[A-Za-z0-9]+@[A-Z-a-z]+\.[A-Za-z]+", correu):
-            self.__correu = correu
+            self.correu = correu
+        else:
+            raise Exception("Correo no sigue el formato")      
         
         if ciutat.isalpha():
-            self.__ciutat = ciutat
+            self.ciutat = ciutat
+        else:
+            raise Exception("Ciudad incorrecto")      
     
     def __str__(self):
-        client = 'Client(ID=' + str(self.id) + ', Nom=' + self.nom + ', Cognom=' + self.cognom + ', Telefon=' + str(self.telefon) + ', Correu=' + self.correu + ', Ciutat=' + self.correu +')'
+        client = 'Client(ID=', str(self.__getattribute__("id")), ', Nom=' + self.__getattribute__("nom"), ', Cognom=', self.__getattribute__("cognom") , ', Telefon=' + str(self.__getattribute__("telefon")), ', Correu=', self.__getattribute__("correu"), ', Ciutat=', self.__getattribute__("ciutat"),')'
         return client
